92958e9fa2ffcf7f77f02a984832aee35766bcb2
IsacPessoa <isacpessoa38@gmail.com>
2024-11-04 18:45:07 -0300

    commitado

---
+++ src/br/com/ilima/Garrafa.java
@@ -0,0 +1,38 @@
+package br.com.ilima;
+
+public class Garrafa {
+
+	// Criando variáveis (propriedades do objeto) 
+	
+	private int volume;
+	private String cor;
+	private String material;
+
+	
+	// Criando métodos get e set 
+	
+	public int getVolume() {
+		return volume;
+	}
+	
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public String getMaterial() {
+		return material;
+	}
+	
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+}
+
